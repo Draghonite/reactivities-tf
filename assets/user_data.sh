bad8e2e28c39c3dffa01c038d52990a611d22c56
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-03-26 16:36:28 -0400

    Update user_data.sh

--- assets/user_data.sh
+++ assets/user_data.sh
@@ -6,3 +6,25 @@ snap alias dotnet-sdk.dotnet dotnet
 snap install dotnet-runtime-60 --classic
 snap alias dotnet-runtime-60.dotnet dotnet
 export DOTNET_ROOT=/snap/dotnet-sdk/current
+
+mkdir -p /var/www/html/api
+mkdir -p /var/www/html/client
+
+cat << EOF > /etc/nginx/sites-available/default
+server {
+    listen        80;
+    location / {
+        proxy_pass         http://127.0.0.1:5000;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade $http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto $scheme;
+    }
+}
+EOF
+
+service nginx start
+nginx -s reload
