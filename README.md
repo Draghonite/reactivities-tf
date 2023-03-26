078cca59867a68e1d6c0cfeda210d7c246dc6338
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-03-26 09:46:17 -0400

    Added a key-pair

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reactivities-tf
  Infrastructure-as-Code for Reactivities using Terraform
+
+### Credits
+- https://lightsail.aws.amazon.com/ls/docs/en_us/articles/understanding-ssh-in-amazon-lightsail
--- main.tf
+++ main.tf
@@ -25,12 +25,18 @@ variable "app_name" {
   default = "Reactivities"
 }
 
+resource "aws_lightsail_key_pair" "lg_key_pair" {
+    name = "lg_key_pair"
+    public_key = file("assets/lg_key_pair.pub")
+}
+
 resource "aws_lightsail_instance" "app_server" {
   name = "${var.app_name}_${var.deploy_env}_${var.region}"
   availability_zone = var.availability_zone
   blueprint_id = "ubuntu_20_04"
   bundle_id = var.bundle_id
   user_data = "${file("assets/user_data.sh")}"
+  key_pair_name = aws_lightsail_key_pair.lg_key_pair.name
   tags = {
     "ApplicationId" = var.app_name
   }
